44a61464e476a44e321f7f34e6c8d1856aaa57e2
Pippi de Bree <pippidebree@gmail.com>
2025-12-06 07:48:48 -0500

    Modeling Files

---
+++ classification.R
@@ -0,0 +1,54 @@
+# Modeling Status 
+
+# Note: we could make better predictions for output with this data. We could find, for example, a metric of inflamatory language, using NLP methods but that is not the aim her. The aim is to compare classification outputs based only on the notes and ratings information. There is likely a large advantage in using LLMs because they may inherently capture this. 
+
+ready_human_notes_ratings <- function(notes, ratings) {
+  
+  # we have to split the categorical variable helpfulness level into three - as this is how our grok output looks (plus this lets us have the reference variable).- we do not encode "NOT_HELPFUL" because this is our reference.
+  ratings_meaned <- ratings %>% mutate(helpfulness_score = case_when(
+    helpfulnessLevel == "HELPFUL" ~ 1, 
+    helpfulnessLevel == "SOMEWHAT_HELPFUL" ~ 0.5,
+    TRUE ~ 0)) %>% group_by(noteId) %>% summarize(
+    across(c("helpfulClear", "helpfulGoodSources", "helpfulUnbiasedLanguage", "notHelpfulIncorrect", "notHelpfulSourcesMissingOrUnreliable", "notHelpfulHardToUnderstand", "notHelpfulArgumentativeOrBiased", "notHelpfulSpamHarassmentOrAbuse", "notHelpfulOpinionSpeculation"), mean, na.rm = TRUE),
+    helpfulnessScore=mean(helpfulness_score))
+  
+  # We keep the note writer's ideas about their note because this may interact with the note ratings. Ie. If both the raters and the note write think the original tweet is misleading this is more likely to end up with a LockedStatus than if there is a discrepancy in opinion. 
+  
+  # Only missing values found in the topic assignment row. 
+  
+  notes_output <- notes %>% mutate(
+    # Turn into dummy variable
+    misinformed_or_misleading = ifelse(classification == "MISINFORMED_OR_POTENTIALLY_MISLEADING",1,0),
+    # Turn output variable into dummy variable
+    locked_helpful = ifelse(lockedStatus == "CURRENTLY_RATED_HELPFUL",1,0)
+  ) %>% dplyr::select(
+    # remove outputs that are not relevant to 
+    -tweetId, -noteAuthorParticipantId, -classification, -month_year,
+    -has_grok_output, -scams, -messi_ronaldo, -gaza_conflict, -ukraine_conflict, 
+    -note_text, -currentDecidedBy, -lockedStatus, -time_created, -keyATM_topic)
+  
+  # need to make sure that the grok_output noteIds are in a separate test set, so that we can compare how they perform compared to the 
+  # training - we can't have them in the training set.
+  
+  # We keep noteId in this set (though it will not be used in the modeling) for data wrangling and comparison of behavior with the different groups.
+  
+  # We take the mean of all the ratings variables (except noteId), but have a more specific method for finding the average of the helpfulness level. This is because there are three categories. And on the *****REFERENCE***** it encodes this as 1, 0.5 and 0. We will do the same thing to our g
+  
+  
+  rated_notes <- left_join(notes_output, ratings_meaned, by="noteId")
+  
+
+  return(rated_notes)
+}
+
+ready_grok <- function(llm_ratings){
+  llm_ratings_output <- llm_ratings %>% 
+    # Turn logical values into numerics
+    mutate(across(where(is.logical), as.integer), 
+           helpfulnessScore = case_when(
+             helpfulnessLevel_helpful == 1 ~ 1, 
+             helpfulnessLevel_somewhathelpful == 1 ~ 0.5, 
+             TRUE ~ 0 
+           )) %>% dplyr::select(-helpfulnessLevel_helpful, - helpfulnessLevel_somewhathelpful, - helpfulnessLevel_nothelpful)
+  return(llm_ratings_output)
+}
